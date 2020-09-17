70502a8d95d8963ab74adc2fd66deb153e8e2495
dj5822 <Dj5822@gmail.com>
2020-09-17 19:36:45 +1200

    Added combobox to select category at practice view.
    Added categoryCB. If the selected category is null, then
    error pops up. Added show question button that switches
    the scene to another pane if a valid category is selected.

--- src/quinzical/controller/PracticeController.java
+++ src/quinzical/controller/PracticeController.java
@@ -1,11 +1,17 @@
 package quinzical.controller;
 
+import javafx.collections.ObservableList;
+
 public class PracticeController {
 	
 	private SceneController sceneController;
 	
 	public PracticeController(SceneController sceneController) {
 		this.sceneController = sceneController;
+	}
+	
+	public ObservableList<String> updateCategoryOptions(ObservableList<String> categoryOptions) {
+		return categoryOptions;
 	}
 	
 	/**
--- src/quinzical/ui/PracticeView.java
+++ src/quinzical/ui/PracticeView.java
@@ -1,10 +1,15 @@
 package quinzical.ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 import quinzical.controller.PracticeController;
 
@@ -14,21 +19,53 @@ public class PracticeView {
 	
 	private Label title;
 	private Button returnToMenuButton;
+	private Button showQuestionButton;
+	
+	private Label categoryLabel;
+	private ObservableList<String> categoryOptions;
+	private ComboBox<String> categoryCB;
 	
 	public PracticeView(PracticeController controller, int width, int height) {
 		
-		GridPane mainPane = new GridPane();
-		main = new Scene(mainPane, width, height);
-		
-		// Initialize buttons and labels.
+		// Setup category pane.
+		GridPane categoryPane = new GridPane();
+		// initialise components.
 		title = new Label("Practice View");
 		returnToMenuButton = new Button("Return to menu");
+		categoryLabel = new Label("Select category: ");
+		categoryOptions = FXCollections.observableArrayList();
+		controller.updateCategoryOptions(categoryOptions);
+		categoryCB = new ComboBox<String>(categoryOptions);
+		showQuestionButton = new Button("Show Question");
+		// add components to pane.
+		categoryPane.add(title, 0, 0, 2, 1);
+		categoryPane.add(categoryLabel, 0, 1);
+		categoryPane.add(categoryCB, 1, 1);
+		categoryPane.add(showQuestionButton, 0, 2, 2, 1);
+		categoryPane.add(returnToMenuButton, 0, 3, 2, 1);
 		
-		// Add buttons and labels to the view.
-		mainPane.add(title, 0, 0);
-		mainPane.add(returnToMenuButton, 0, 1);
+		// Setup answer pane.
+		GridPane answerPane = new GridPane();
+		
+		// start at category pane.
+		main = new Scene(categoryPane, width, height);
 		
 		// Button functionality.
+		showQuestionButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				if (categoryCB.getValue() == null) {
+					Alert errorAlert = new Alert(AlertType.ERROR);
+					errorAlert.setTitle("Error encountered");
+					errorAlert.setHeaderText("You must select a category");					
+					errorAlert.showAndWait();
+				}
+				else {
+					main.setRoot(answerPane);
+				}
+			}
+		});
+		
 		returnToMenuButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent arg0) {
