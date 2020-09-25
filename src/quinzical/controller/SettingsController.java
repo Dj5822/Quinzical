8e9b3b1e78aafbd4f6ad449243144a62d5d26459
dj5822 <Dj5822@gmail.com>
2020-09-25 16:17:25 +1200

    Added components to the settings view
    Added sliders, buttons, and labels to settings view.
    Settings view is used to adjust speech speed.

--- src/quinzical/controller/SettingsController.java
+++ src/quinzical/controller/SettingsController.java
@@ -3,9 +3,21 @@ package quinzical.controller;
 public class SettingsController {
 	
 	private SceneController sceneController;
+	private String testText = "What is the meaning of life?";
+	private int speed;
 	
 	public SettingsController(SceneController sceneController) {
 		this.sceneController = sceneController;
+	}
+	
+	public void testSpeech() {
+		AudioTask task1 = new AudioTask(testText);
+		Thread thread1 = new Thread(task1);
+		thread1.start();
+	}
+	
+	public String getTestText() {
+		return testText;
 	}
 	
 	/**
--- src/quinzical/ui/SettingsView.java
+++ src/quinzical/ui/SettingsView.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import quinzical.controller.SettingsController;
 
@@ -13,6 +14,10 @@ public class SettingsView {
 	private Scene main;
 	
 	private Label title;
+	private Label speechSpeedLabel;
+	private Slider speechSpeedSlider;
+	private Label testLabel;
+	private Button testButton;
 	private Button returnToMenuButton;
 	
 	public SettingsView(SettingsController controller, int width, int height) {
@@ -22,13 +27,28 @@ public class SettingsView {
 		
 		// Initialize buttons and labels.
 		title = new Label("Settings");
+		speechSpeedLabel = new Label("Speech speed: ");
+		speechSpeedSlider = new Slider();
+		testLabel = new Label(controller.getTestText());
+		testButton = new Button("Test");
 		returnToMenuButton = new Button("Return to menu");
 		
 		// Add buttons and labels to the view.
-		mainPane.add(title, 0, 0);
-		mainPane.add(returnToMenuButton, 0, 1);
+		mainPane.add(title, 0, 0, 2, 1);
+		mainPane.add(testLabel, 0, 1);
+		mainPane.add(testButton, 1, 1);
+		mainPane.add(speechSpeedLabel, 0, 2);
+		mainPane.add(speechSpeedSlider, 1, 2);
+		mainPane.add(returnToMenuButton, 0, 3, 2, 1);
 		
 		// Button functionality
+		testButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				controller.testSpeech();
+			}
+		});		
+		
 		returnToMenuButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent arg0) {
