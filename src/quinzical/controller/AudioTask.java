e5be3c7932ec3a02655eeb0be771c09ad2ba1745
dj5822 <Dj5822@gmail.com>
2020-09-26 18:19:00 +1200

    Finished implementing settingsController
    User is now able to adjust tts speed throught the settings view.

--- src/quinzical/controller/AudioTask.java
+++ src/quinzical/controller/AudioTask.java
@@ -8,19 +8,22 @@ import javafx.concurrent.Task;
 public class AudioTask extends Task<Object> {
 	
 	private String text;
+	private double speed;
 	
-	public AudioTask(String text) {
+	public AudioTask(String text, double speed) {
 		this.text = text;
+		this.speed = speed;
 	}
 
 	@Override
 	protected Object call() throws Exception {
 		try {
-			ProcessBuilder builder = new ProcessBuilder("festival", "--tts");			
+			ProcessBuilder builder = new ProcessBuilder("festival");			
 			Process process = builder.start();
 			OutputStream in = process.getOutputStream();
 			PrintWriter stdin = new PrintWriter(in);
-			stdin.println(text);
+			stdin.println("(Parameter.set `Duration_Stretch " + 1/speed + ")");
+			stdin.println("(SayText \"" + text + "\")");
 			stdin.close();
 			process.waitFor();
 			
